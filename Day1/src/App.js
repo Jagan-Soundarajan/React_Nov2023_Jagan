7cc423782603345cdcfe39f56528d55bc398856f
Jagan-Soundarajan <150216338+Jagan-Soundarajan@users.noreply.github.com>
2023-11-08 10:32:25 +0530

    Add files via upload

---
+++ Day1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Helloworld from "./component/helloworld";
+
+function App() {
+  return (
+    <div className="App">
+      <Helloworld />
+    </div>
+  );
+}
+
+export default App;
---
+++ Day1/src/component/helloworld.js
@@ -0,0 +1,10 @@
+function Helloworld() {
+    return(
+        <div>
+            <h1>
+                welcome to skcet
+            </h1>
+        </div>
+    );
+    }
+export default Helloworld;
